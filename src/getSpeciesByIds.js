f0232469979952ec23cab3f2f8d065e7182ba227
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-02 19:01:06 -0300

    Atualização Lint Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,7 @@
-const species = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const speciesReference = species.filter((idSpecies) =>
-    ids.find((acessId) => idSpecies.id === acessId));
-
-  return speciesReference;
+  return species.filter((idSpecies) => ids.find((acessId) => idSpecies.id === acessId));
 }
 
 module.exports = getSpeciesByIds;
