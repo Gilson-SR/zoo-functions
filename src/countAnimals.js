d28b496147020f409782b873009cda823744e1ff
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-03 11:57:26 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// let [lions, tigre, urso, pinguim, lontras, sapos, cobras, elefantes, girafas] = species;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const objAnimals = {};
+    data.species.forEach((animals) => {
+      objAnimals[animals.name] = animals.residents.length;
+    });
+    return objAnimals;
+  }
+  if (animal.sex) {
+    const specieSex = species.find((element) => element.name === animal.specie);
+    return specieSex.residents.filter((arrayElement) => arrayElement.sex === animal.sex).length;
+  }
+  return species.find((element) => element.name === animal.specie).residents.length;
 }
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
