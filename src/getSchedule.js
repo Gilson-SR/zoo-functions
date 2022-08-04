60618388465f8bb10fd81dbfe77bea2ed818e33a
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-04 12:57:48 -0300

    Implementação da função infoOpening

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,23 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const week = Object.keys(hours)
+
+
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+}
+
+function infoOpening() {
+  const openingHours = week.reduce((acc, curr) => {
+    acc[curr] = {
+      officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+      exhibition: species.filter((element) => element.availability.includes(curr)).map((arraySpecie) => arraySpecie.name),
+    };
+    return acc;
+  }, {});
+  
 }
 
 module.exports = getSchedule;
