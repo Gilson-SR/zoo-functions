6ef4594569307495a62911b142a44a15faba6b60
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-04 13:20:04 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,13 @@
 const { hours, species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
-const week = Object.keys(hours)
-
-
-
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const week = Object.keys(hours);
 
 function infoOpening() {
   const openingHours = week.reduce((acc, curr) => {
     acc[curr] = {
       officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
-      exhibition: species.filter((element) => element.availability.includes(curr)).map((arraySpecie) => arraySpecie.name),
+      exhibition: species.filter((element) =>
+        element.availability.includes(curr)).map((arraySpecie) => arraySpecie.name),
     };
     return acc;
   }, {});
@@ -22,6 +16,21 @@ function infoOpening() {
     exhibition: 'The zoo will be closed!',
   };
   return openingHours;
+}
+
+function getSchedule(scheduleTarget) {
+  if (scheduleTarget === undefined) {
+    return infoOpening();
+  }
+
+  const sameNameVerify = species.some((specie) => specie.name === scheduleTarget);
+  if (!sameNameVerify && !week.includes(scheduleTarget)) {
+    return infoOpening();
+  }
+  if (week.includes(scheduleTarget)) {
+    return { [scheduleTarget]: infoOpening()[scheduleTarget] };
+  }
+  return species.find((weekDay) => weekDay.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
