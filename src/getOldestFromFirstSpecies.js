055ec9e3344e986874f236463064f66aaa3d70fd
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-08-03 23:05:25 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecieId = employees.find((element) => element.id === id).responsibleFor[0];
+  const specieObj = species.find((specie) => specie.id === firstSpecieId).residents;
+  return Object.values(specieObj.sort((a, b) => a.age - b.age)[specieObj.length - 1]);
 }
 
 module.exports = getOldestFromFirstSpecies;
